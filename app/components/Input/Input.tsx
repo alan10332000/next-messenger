1384872efd2c37d1b3997e51e4cdd2a613325cb2
Alan Chou <alan10332000@gmail.com>
2023-05-14 14:17:56 +0800

    feat: add common components - input pattern

--- app/components/Input/Input.tsx
+++ app/components/Input/Input.tsx
@@ -7,13 +7,14 @@ interface InputProps {
   label: string
   id: string
   type?: string
+  pattern?: RegExp
   required?: boolean
   register: UseFormRegister<FieldValues>
   errors: FieldErrors
   disabled?: boolean
 }
 
-const Input: React.FC<InputProps> = ({ label, id, register, required, errors, type = 'text', disabled }) => {
+const Input: React.FC<InputProps> = ({ label, id, type = 'text', pattern, required, register, errors, disabled }) => {
   return (
     <div>
       <label htmlFor={id} className="block text-sm font-medium leading-6 text-gray-900">
@@ -25,7 +26,7 @@ const Input: React.FC<InputProps> = ({ label, id, register, required, errors, ty
           type={type}
           autoComplete={id}
           disabled={disabled}
-          {...register(id, { required })}
+          {...register(id, { required, pattern })}
           className={clsx(
             `form-input block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6`,
             errors[id] && 'focus:ring-rose-500',
