382f6be9c76b9cdc9fd718d392bc458f28c0fce0
Alan Chou <alan10332000@gmail.com>
2023-05-14 14:18:22 +0800

    feat: add auth page - auth form

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image'
 
+import AuthForm from './components/AuthForm'
+
 const Auth = () => {
   return (
     <div className="flex min-h-full flex-col justify-center bg-gray-100 py-12 sm:px-6 lg:px-8">
@@ -9,6 +11,7 @@ const Auth = () => {
         </div>
         <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Sign in to your account</h2>
       </div>
+      <AuthForm />
     </div>
   )
 }
