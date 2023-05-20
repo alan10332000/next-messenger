42c9b54455ec0e0b1898cd68fd34a1124938bd18
Alan Chou <alan10332000@gmail.com>
2023-05-20 17:00:58 +0800

    feat: add loading UI

--- app/users/components/UserBox.tsx
+++ app/users/components/UserBox.tsx
@@ -4,6 +4,7 @@ import { useRouter } from 'next/navigation'
 import { useState, useCallback } from 'react'
 
 import Avatar from '@/app/components/Avatar'
+import LoadingModal from '@/app/components/Modal/LoadingModal'
 
 interface UserBoxProps {
   data: User
@@ -27,20 +28,23 @@ const UserBox: React.FC<UserBoxProps> = ({ data }) => {
   }, [data, router])
 
   return (
-    <div
-      onClick={handleClick}
-      className="relative flex w-full cursor-pointer items-center space-x-3 rounded-lg bg-white p-3 transition hover:bg-neutral-100"
-    >
-      <Avatar user={data} />
-      <div className="min-w-0 flex-1">
-        <div className="focus:outline-none">
-          <span className="absolute inset-0" aria-hidden="true" />
-          <div className="mb-1 flex items-center justify-between">
-            <p className="text-sm font-medium text-gray-900">{data.name}</p>
+    <>
+      {isLoading && <LoadingModal />}
+      <div
+        onClick={handleClick}
+        className="relative flex w-full cursor-pointer items-center space-x-3 rounded-lg bg-white p-3 transition hover:bg-neutral-100"
+      >
+        <Avatar user={data} />
+        <div className="min-w-0 flex-1">
+          <div className="focus:outline-none">
+            <span className="absolute inset-0" aria-hidden="true" />
+            <div className="mb-1 flex items-center justify-between">
+              <p className="text-sm font-medium text-gray-900">{data.name}</p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
